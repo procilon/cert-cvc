6ca3d3a31f5248e93ff0211a9a7a36fc0c1bed00
Max Fichtelmann <max.fichtelmann@procilon.de>
2017-10-30 18:05:58 +0100

    value handling for providing access to the value in TLV

---
+++ src/main/java/org/ejbca/cvc/internal/tlv/Value.java
@@ -0,0 +1,74 @@
+package org.ejbca.cvc.internal.tlv;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Simple {@link ByteBuffer} value wrapper.
+ * 
+ * <p>
+ * NOT THREADSAFE - do not use across threads!
+ * 
+ * @author fichtelmannm
+ *
+ */
+public class Value {
+    private final ByteBuffer bytes;
+
+    /**
+     * Wraps the provided bytes in a value. Intended to use in TLV context.
+     * 
+     * @param bytes
+     *            the bytes of this value.
+     */
+    public Value(ByteBuffer bytes) {
+	bytes.mark();
+	this.bytes = bytes;
+    }
+
+    /**
+     * Get access to a duplicate of the underlying {@link ByteBuffer}.
+     * 
+     * @return a duplicate of the underlying {@link ByteBuffer}.
+     * @see ByteBuffer#duplicate()
+     */
+    public ByteBuffer bytes() {
+	return bytes.duplicate();
+    }
+
+    /**
+     * Decodes this value with the provided charset.
+     * 
+     * @param charset
+     *            the charset to decode the value.
+     * @return the string representation of this value.
+     */
+    public String asString(Charset charset) {
+	CharBuffer result = charset.decode(bytes);
+	bytes.reset();
+	return result.toString();
+    }
+
+    /**
+     * Decodes this value as ascii string.
+     * 
+     * @return the string representation of this value.
+     */
+    public String asString() {
+	return asString(StandardCharsets.US_ASCII);
+    }
+
+    /**
+     * Copies the content of this value into a new byte array.
+     * 
+     * @return a bytearray with the content of this value.
+     */
+    public byte[] asBytes() {
+	byte[] binaryValue = new byte[bytes.remaining()];
+	bytes.get(binaryValue);
+	bytes.reset();
+	return binaryValue;
+    }
+}
---
+++ src/test/java/org/ejbca/cvc/internal/tlv/ValueTest.java
@@ -0,0 +1,68 @@
+package org.ejbca.cvc.internal.tlv;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import org.bouncycastle.util.encoders.Hex;
+import org.junit.Test;
+
+public class ValueTest {
+    @Test
+    public void encodeString() throws Exception {
+	Charset charset = StandardCharsets.US_ASCII;
+
+	String testString = "Some test text";
+	ByteBuffer bytes = charset.encode(testString);
+
+	Value value = new Value(bytes.asReadOnlyBuffer());
+
+	assertEquals(testString, value.asString());
+    }
+
+    @Test
+    public void encodeUTF8String() throws Exception {
+	Charset charset = StandardCharsets.UTF_8;
+
+	String testString = "(╯°□°)╯︵ ┻━┻";
+	ByteBuffer bytes = charset.encode(testString);
+
+	Value value = new Value(bytes.asReadOnlyBuffer());
+
+	assertEquals(testString, value.asString(charset));
+    }
+
+    @Test
+    public void encodeBytes() throws Exception {
+	byte[] rawValue = Hex.decode("CAFEBABE");
+
+	Value value = new Value(ByteBuffer.wrap(rawValue));
+
+	assertEquals(Hex.toHexString(rawValue), Hex.toHexString(value.asBytes()));
+    }
+
+    @Test
+    public void repeatedEncode() throws Exception {
+	String testString = "Some test text";
+	byte[] testBytes = testString.getBytes(StandardCharsets.US_ASCII);
+
+	Value value = new Value(ByteBuffer.wrap(testBytes));
+
+	assertEquals(testString, value.asString());
+	assertEquals(testString, value.asString());
+	assertEquals(Hex.toHexString(testBytes), Hex.toHexString(value.asBytes()));
+	assertEquals(Hex.toHexString(testBytes), Hex.toHexString(value.asBytes()));
+    }
+
+    @Test
+    public void safeAccessToBuffer() throws Exception {
+	byte[] rawValue = Hex.decode("CAFEBABE");
+
+	Value value = new Value(ByteBuffer.wrap(rawValue));
+	value.bytes().position(1);
+
+	assertEquals(Hex.toHexString(rawValue), Hex.toHexString(value.asBytes()));
+    }
+}
